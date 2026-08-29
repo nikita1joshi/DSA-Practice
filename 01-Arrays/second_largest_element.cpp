908bfddeca1c2775a8cac20ab52746f83cb6f569
Nikita Joshi <nikitajoshi1630@gmail.com>
2026-08-29 17:32:20 +0530

    Added second largest element problem

---
+++ 01-Arrays/second_largest_element.cpp
@@ -0,0 +1,34 @@
+// Problem: Find the Second Largest Element in an Array
+// Difficulty: Easy
+// Approach: Single pass, track largest and second largest together
+
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+       int largest=nums[0];
+       int slarge=INT_MIN;
+       if(nums.size()<2){
+            cout<<"Only one element is there in nums"<<endl;
+            return 0;
+       }
+       if(nums.size()>=2){
+            for(int i=1; i<nums.size(); i++){
+                if(nums[i]>largest){
+                    slarge=largest;
+                    largest = nums[i];
+                
+                }
+                else if(nums[i]<largest && nums[i]>slarge){
+                    slarge=nums[i];
+                }
+
+            } 
+            if(slarge==INT_MIN){
+                    return -1;
+                }  
+       
+       }
+        return slarge;
+      
+    }
+};
